1e418d6dc3e599d38d6c6a3b5f4b9afbd9076867
Azeez <azeezahemedkhan@gmail.com>
2026-07-31 22:21:52 +0530

    changes

--- api/controller/product.controller.js
+++ api/controller/product.controller.js
@@ -78,7 +78,7 @@ export const deleteProductsById3 = async (req, res) => {
 };
 
 
-export const deleteProductsById06 = async (req, res) => {
+export const deleteProductsById07 = async (req, res) => {
   try {
     const { id } = req.params;
     const product = await ProductModel.findByIdAndDelete(id);
