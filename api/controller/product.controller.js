5c599a553517cf0ea62f11f449c79ec4e7cd208e
Azeez <azeezahemedkhan@gmail.com>
2026-07-31 22:19:36 +0530

    "improved stablity"

--- api/controller/product.controller.js
+++ api/controller/product.controller.js
@@ -78,7 +78,7 @@ export const deleteProductsById3 = async (req, res) => {
 };
 
 
-export const deleteProductsById02 = async (req, res) => {
+export const deleteProductsById03 = async (req, res) => {
   try {
     const { id } = req.params;
     const product = await ProductModel.findByIdAndDelete(id);
